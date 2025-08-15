01f5e7d0bb8127a1b6915980327f1fdf7dac8b88
اسمك <@.com>
2025-08-16 01:19:08 +0300

    base changes

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -4,30 +4,35 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is set to '/'
+  base: "/static/", // Make sure this is set to '/'
   build: {
-    outDir: 'dist', // Ensure this is `dist`
+    outDir: "dist", // Ensure this is `dist`
+    emptyOutDir: true,
   },
   server: {
     port: 8080,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
+          proxy.on("error", (err, req, res) => {
+            console.log("proxy error", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          proxy.on("proxyRes", (proxyRes, req, res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
           });
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
